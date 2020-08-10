df4c383e037afa0fdc3b0970a3f19e6313380637
Matthew Gleich <matthewgleich@gmail.com>
2020-08-10 00:20:04 -0400

    📄 Add MIT license

--- src/extension.ts
+++ src/extension.ts
@@ -4,25 +4,23 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate({ subscriptions }: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "emoji-clock" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'emoji-clock.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
+  console.log(vscode.workspace.getConfiguration());
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from emoji-clock!');
-    }
+  let clock = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    1000000000000000000000000000000000000000
   );
-
-  context.subscriptions.push(disposable);
+  subscriptions.push(clock);
+  clock.tooltip = 'Hello World!';
+  subscriptions.push(clock);
+  clock.text = 'Hello World!';
+  subscriptions.push(clock);
+  clock.show();
 }
 
 // this method is called when your extension is deactivated
